a7ecdbc84e3d406bcea8f1abe496866eb57069f5
Woonjang Ahn <woonjangahn@gmail.com>
2020-05-15 18:27:03 +0900

    refactor : 이벤트 핸들러 함수 분리

--- js/components/TodoList.js
+++ js/components/TodoList.js
@@ -1,17 +1,21 @@
 import template from "./TodoItem.js";
 
+const addEventHandlers = (event, toggleCompleteHandler, deleteHandler) => {
+  if (event.target.classList.contains("toggle")) {
+    const id = event.target.closest("li").dataset.id;
+    toggleCompleteHandler(id);
+  }
+  if (event.target.classList.contains("destroy")) {
+    const id = event.target.closest("li").dataset.id;
+    deleteHandler(id);
+  }
+};
+
 export default class TodoList {
   constructor(items, { toggleCompleteHandler, deleteHandler }) {
     this.$todoList = document.querySelector("#todo-list");
     this.$todoList.addEventListener("click", event => {
-      if (event.target.classList.contains("toggle")) {
-        const id = event.target.closest("li").dataset.id;
-        toggleCompleteHandler(id);
-      }
-      if (event.target.classList.contains("destroy")) {
-        const id = event.target.closest("li").dataset.id;
-        deleteHandler(id);
-      }
+      addEventHandlers(event, toggleCompleteHandler, deleteHandler);
     });
     this.render(items);
   }
