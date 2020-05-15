51e4e96a9673a91db225a6e9a75e84c5fc18fcea
Woonjang Ahn <woonjangahn@gmail.com>
2020-05-15 18:18:48 +0900

    feat : todo list 아이템별로 complete 토글 기능 구현

--- js/TodoApp.js
+++ js/TodoApp.js
@@ -7,19 +7,21 @@ import { DEFAULT_DATA } from "./util/data.js";
 export default class TodoApp {
   constructor() {
     this.items = DEFAULT_DATA || [];
-    new TodoList(this.items);
+    this.todoList = new TodoList(this.items, { toggleCompleteHandler: this.handleToggleComplete.bind(this) });
     new TodoInput({ addTodoHandler: this.handleAddTodo.bind(this) });
   }
 
   setState(items) {
     this.items = items;
-    new TodoList(this.items);
+    this.todoList.render.call(this.todoList, this.items);
   }
 
   handleAddTodo(content) {
     this.setState(this.items.concat({ id: getUUID(), title: content }));
   }
+
+  handleToggleComplete(id) {
+    this.setState(this.items.map(item => item.id !== id ? item
+      : { ...item, status: item.status === "completed" ? null : "completed" }));
+  }
 }
-
-
-
--- js/components/TodoList.js
+++ js/components/TodoList.js
@@ -1,8 +1,14 @@
 import template from "./TodoItem.js";
 
 export default class TodoList {
-  constructor(items) {
+  constructor(items, { toggleCompleteHandler }) {
     this.$todoList = document.querySelector("#todo-list");
+    this.$todoList.addEventListener("click", event => {
+      if (event.target.classList.contains("toggle")) {
+        const id = event.target.closest("li").dataset.id;
+        toggleCompleteHandler(id);
+      }
+    });
     this.render(items);
   }
 
