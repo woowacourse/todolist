678fe0cd5753022481a69bc61e3d53162b33ab90
Yoonseo Nam <turtlenam0928@gmail.com>
2020-05-17 01:36:50 +0900

    feat: todo list 완료 기능 구현

--- js/todo/TodoList.js
+++ js/todo/TodoList.js
@@ -1,7 +1,9 @@
 import { itemTemplate } from '../template/template.js';
+import { EVENT_TYPE } from '../utils/constants.js';
 
 export default function TodoList() {
   this.$todoList = document.querySelector('#todo-list');
+  this.$todoList.addEventListener(EVENT_TYPE.CLICK, e => this.onClickCheckBox(e))
 
   this.setState = updatedTodoItems => {
     this.render(updatedTodoItems);
@@ -10,5 +12,19 @@ export default function TodoList() {
   this.render = items => {
     const template = items.map(itemTemplate);
     this.$todoList.innerHTML = template.join("");
+  }
+
+  this.onClickCheckBox = e => {
+    const $target = e.target;
+    if ($target && $target.nodeName === 'INPUT') {
+      const $list = $target.closest('li');
+      const isCompleted = $list.classList.contains('completed');
+
+      if (!isCompleted) {
+        $list.classList.add('completed');
+      } else {
+        $list.classList.remove('completed');
+      }
+    }
   }
 }
