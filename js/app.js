f04ae563bbb9f6ae1f3f0a73af3b43b488a0f49c
dd <dqrd123@gmail.com>
2020-05-16 19:37:36 +0900

    feat: 기능 구현2
    - 상태관리 방식으로 재구현
    - todo list에 todoItem을 키보드로 입력하여 추가하기
    - todo list의 체크박스를 클릭하여 complete 상태로 변경
    - todo list의 x버튼을 이용해서 해당 엘리먼트를 삭제
    - todo list를 더블클릭했을 때 input 모드로 변경
    - todo list의 item갯수를 count한 갯수를 리스트의 하단에 보여주기

--- js/app.js
+++ js/app.js
@@ -1,45 +1,83 @@
 function TodoApp() {
   this.todoItems = [];
 
-  this.todoList = new TodoList(event => {
-    const $completeTarget = event.target;
-    if ($completeTarget.classList.contains("toggle")) {
-      $completeTarget.closest("li").classList.toggle("completed");
-    }
-    if ($completeTarget.classList.contains("destroy")) {
-      const child = $completeTarget.closest("li");
-      child.parentNode.removeChild(child);
-    }
-  }, event => {
-    const $editTarget = event.target;
-    $editTarget.closest("li").classList.toggle("editing", true);
-  }, event => {
-    if (event.key === "Escape") {
-      const $editTarget = event.target.closest("li");
-      if ($editTarget.classList.contains("editing")) {
-        $editTarget.classList.toggle("editing", false);
-      }
-    }
-  });
+  this.createdCount = 0;
 
-  this.setState = updatedItems => {
+  this.setState = (updatedItems, updatedItemCount) => {
+
     this.todoItems = updatedItems;
-    this.todoList.setState(this.todoItems);
+    this.createdCount = updatedItemCount;
+    this.todoList.render(updatedItems);
+    this.todoCount.render(updatedItemCount + 1);
   };
 
-  new TodoInput(contents => {
-    const newTodoItem = new TodoItem(contents);
+  const onToggle = (event, id) => {
+    const index = this.todoItems.findIndex(item => item.id === id);
+    const todoItem = this.todoItems[index];
+    if (todoItem.status === "completed") {
+      todoItem.status = "default";
+    } else if (todoItem.status === "default") {
+      todoItem.status = "completed";
+    }
+    this.todoItems[index] = todoItem;
+    this.setState(this.todoItems, this.createdCount);
+  };
+
+  const onDelete = (event, id) => {
+    const index = this.todoItems.findIndex(item => item.id === id);
+    this.todoItems.splice(index, 1)
+    this.setState(this.todoItems, this.createdCount);
+  }
+
+  const onDblClick = (event, id) => {
+    const index = this.todoItems.findIndex(item => item.id === id);
+    const todoItem = this.todoItems[index];
+    if (todoItem.status === "default") {
+      todoItem.status = "editing";
+    }
+    this.todoItems[index] = todoItem;
+    this.setState(this.todoItems, this.createdCount);
+  };
+
+  const onRollback = (event, id) => {
+    const index = this.todoItems.findIndex(item => item.id === id);
+    const todoItem = this.todoItems[index];
+    if (todoItem.status === "editing") {
+      todoItem.status = "default";
+      this.todoItems[index] = todoItem;
+      this.setState(this.todoItems, this.createdCount);
+    }
+  }
+  const onCommit = (event, id, content) => {
+    const index = this.todoItems.findIndex(item => item.id === id);
+    const todoItem = this.todoItems[index];
+    if (todoItem.status === "editing") {
+      todoItem.content = content;
+      todoItem.status = "default";
+      this.todoItems[index] = todoItem;
+      this.setState(this.todoItems, this.createdCount);
+    }
+  }
+
+  const onAdd = (contents) => {
+    const newTodoItem = new TodoItem(++this.createdCount, contents, "default");
     this.todoItems.push(newTodoItem);
-    this.setState(this.todoItems);
-  });
+    this.setState(this.todoItems, this.createdCount);
+  };
+
+  this.todoList = new TodoList(onToggle, onDelete, onDblClick, onRollback, onCommit);
+
+  this.todoCount = new TodoCount();
+
+  new TodoInput(onAdd);
+
+  new TodoCount(this.createdCount);
 }
 
 function TodoInput(onAdd) {
   const $todoInput = document.querySelector("#new-todo-title");
-
-  $todoInput.addEventListener("keyup", event => this.addTodoItem(event));
-
-  this.addTodoItem = event => {
+  $todoInput.addEventListener("keyup", (event) => this.addTodoItem(event));
+  this.addTodoItem = (event) => {
     const $newTodoTarget = event.target;
     if (this.isValid(event, $newTodoTarget.value)) {
       onAdd($newTodoTarget.value);
@@ -48,42 +86,90 @@ function TodoInput(onAdd) {
   };
 
   this.isValid = function (event, s) {
-    return !!(event.key === "Enter" && s);
+    return !!(event.key === "Enter" && s.trim());
   };
 }
 
-function TodoList(handler, editHandler, editCompleteHandler) {
+function TodoList(onToggle, onDelete, onDblClick, onRollback, onCommit) {
   this.$todoList = document.querySelector("#todo-list");
 
-  this.$todoList.addEventListener('click', event => handler(event));
-  this.$todoList.addEventListener('dblclick', event => editHandler(event));
-  this.$todoList.addEventListener('keyup', event => editCompleteHandler(event))
+  this.$todoList.addEventListener("click", (event) => this.click(event));
 
-  this.setState = updatedTodoItems => {
-    this.todoItems = updatedTodoItems;
-    this.render(this.todoItems);
+  this.$todoList.addEventListener("dblclick", (event) => this.startEdit(event));
+
+  this.$todoList.addEventListener("keyup", (event) => this.finishEdit(event));
+
+  this.click = (event) => {
+    const $target = event.target;
+    const id = Number($target.closest("li").getAttribute("id"));
+    if ($target.classList.contains("toggle")) {
+      onToggle(event, id);
+    } else if ($target.classList.contains("destroy")) {
+      onDelete(event, id);
+    }
   };
 
-  this.render = items => {
-    const template = items.map(todoItemTemplate);
-    this.$todoList.innerHTML = template.join("");
-  };
+  this.startEdit = (event) => {
+    const $target = event.target;
+    const id = Number($target.closest("li").getAttribute("id"));
+    if ($target.classList.contains("label")) {
+      onDblClick(event, id);
+    }
+  }
+
+  this.finishEdit = (event) => {
+    const $target = event.target;
+    const id = Number($target.closest("li").getAttribute("id"));
+    if ($target.classList.contains("edit")) {
+      if (event.key === "Enter") {
+        onCommit(event, id, $target.value);
+      } else if (event.key === "Escape") {
+        onRollback(event, id);
+      }
+    }
+  }
+
+  this.render = (items) => {
+    this.$todoList.innerHTML = items.map(todoItemTemplate).join("");
+  }
 }
 
-function TodoItem(contents) {
-  this.content = contents;
+function TodoItem(id, content, status) {
+  this.id = id;
+  this.content = content;
+  this.status = status;
 }
 
 function todoItemTemplate(todoItem) {
-  return `<li>
-        <div class="view">
-          <input class="toggle" type="checkbox">
-          <label class="label">${todoItem.content}</label>
-          <button class="destroy"></button>
-        </div>
-        <input class="edit" value="새로운 타이틀">
-      </li>`
+  if (todoItem.status === "completed") {
+    return `<li class="${todoItem.status}" id="${todoItem.id}">
+          <div class="view">
+            <input class="toggle" type="checkbox" checked>
+            <label class="label">${todoItem.content}</label>
+            <button class="destroy"></button>
+          </div>
+          <input class="edit" value="${todoItem.content}">
+        </li>`;
+  } else {
+    return `<li class="${todoItem.status}" id="${todoItem.id}">
+          <div class="view">
+            <input class="toggle" type="checkbox">
+            <label class="label">${todoItem.content}</label>
+            <button class="destroy"></button>
+          </div>
+          <input class="edit" value="${todoItem.content}">
+        </li>`;
+  }
 }
 
+function TodoCount() {
+
+  this.$todoCount = document.querySelector(".count-container");
+
+  this.render = (count) => {
+    this.$todoCount.innerHTML = `총 <strong>${count}</strong> 개`;
+  }
+
+}
 
 new TodoApp();
