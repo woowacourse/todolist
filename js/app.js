4ae3eddba8c5ba532990c21731d0786803108714
KIMSIYOUNG <tldud2404@gmail.com>
2020-05-16 17:35:09 +0900

    feat : 완료 표시 및 삭제 기능 구현

--- js/app.js
+++ js/app.js
@@ -1,63 +1,129 @@
+function TodoItem(content, status) {
+  this.content = content;
+  this.status = status;
+  this.reverseStatus = () => {
+    if (this.status === "completed") {
+      this.status = "ready";
+    } else if (this.status === "ready") {
+      this.status = "completed";
+    }
+  }
+  this.changeEditingMode = () => {
+    if (this.status === "ready") {
+      this.status = "editing";
+    }
+  }
+}
+
 function TodoApp() {
   this.items = [];
 
   this.setState = updatedItem => {
     this.items = updatedItem;
-    const todoList = new TodoList();
-    todoList.setState(this.items);
+    this.todoList.render(this.items);
   };
-  new todoItem({
-    onAdd: contents => {
-      const newItems = new TodoItem(contents, "ready");
-      this.items.push(newItems);
-      this.setState(this.items);
-    }
-  });
+
+  const onToggle = (event, index) => {
+    const selectedItem = this.items[index];
+    selectedItem.reverseStatus();
+    this.items[index] = selectedItem;
+    this.setState(this.items);
+  }
+
+  const onDelete = (event, index) => {
+    this.items.splice(index, 1);
+    this.setState(this.items);
+  }
+
+  const onDoubleClick = (event, index) => {
+    const selectedItem = this.items[index];
+    selectedItem.changeEditingMode();
+    this.items[index] = selectedItem;
+    this.setState(this.items);
+  }
+
+  const onInputContents = (contents) => {
+    const newItem = new TodoItem(contents, "ready");
+    this.items.push(newItem);
+    this.setState(this.items);
+  }
+
+  this.todoList = new TodoList(onToggle, onDelete, onDoubleClick);
+  this.todoInput = new TodoInput(onInputContents)
 }
 
-function TodoItem(content, status) {
-  this.content = content;
-  this.status = status;
-}
-
-function todoItem({ onAdd }) {
+function TodoInput(onInputContents) {
   const $todoInput = document.querySelector("#new-todo-title");
+  $todoInput.addEventListener("keyup", (event) => this.addItem(event));
 
-  $todoInput.addEventListener("keyup", event => this.addItemListener(event));
-
-  this.addItemListener = event => {
-    const $newTodoTarget = event.target;
-    if (this.isValid(event, $newTodoTarget.value)) {
-      onAdd($newTodoTarget.value);
-      $newTodoTarget.value = "";
+  this.addItem = event => {
+    const $target = event.target;
+    if (this.isValid(event, $target.value)) {
+      onInputContents($target.value);
+      $target.value = "";
     }
-  };
-
-  this.isValid = (eventType, value) => {
-    return !!(eventType.key === "Enter" && value.trim());
-  };
+  }
+  this.isValid = (event, value) => {
+    return !!(event.key === "Enter" && value.trim());
+  }
 }
 
-function TodoList() {
-  this.setState = updatedItem => {
-    this.items = updatedItem;
-    this.render(this.items);
+function TodoList(onToggle, onDelete, onDoubleClick) {
+  this.$todoList = document.querySelector("#todo-list");
+  this.$todoList.addEventListener("click", (event) => this.click(event));
+  this.$todoList.addEventListener("dblclick", (event) => this.doubleClick(event));
+  this.click = event => {
+    const $target = event.target;
+    if ($target.classList.contains("toggle")) {
+      const index = findIndex(event, $target);
+      onToggle(event, index);
+    } else if ($target.classList.contains("destroy")) {
+      const index = findIndex(event, $target);
+      onDelete(event, index);
+    }
+  };
+  this.doubleClick = event => {
+    const $target = event.target;
+    if ($target.classList.contains("label")) {
+      const index = findIndex(event, $target);
+      onDoubleClick(event, index);
+    }
   }
   this.render = items => {
-    this.$todoList = document.querySelector("#todo-list");
     this.$todoList.innerHTML = items.map(todoItemTemplate).join("");
-  };
+  }
 }
 
-function todoItemTemplate(item) {
-  return `<li class="${item.status}">
-    <div class="view">
-    <input class="toggle" type="checkbox">
-    <label class="label">${item.content}</label>
-    <button class="destroy"></button>
-    </div>
-    <input class="edit" value="새로운 타이틀">
-    </li>`
+function todoItemTemplate(todoItem) {
+  if (todoItem.status === "completed") {
+    return `<li class="${todoItem.status}">
+          <div class="view">
+            <input class="toggle" type="checkbox" checked>
+            <label class="label">${todoItem.content}</label>
+            <button class="destroy"></button>
+          </div>
+          <input class="edit" value="${todoItem.content}">
+        </li>`;
+  } else {
+    return `<li class="${todoItem.status}">
+          <div class="view">
+            <input class="toggle" type="checkbox">
+            <label class="label">${todoItem.content}</label>
+            <button class="destroy"></button>
+          </div>
+          <input class="edit" value="${todoItem.content}">
+        </li>`;
+  }
+}
+
+function findIndex(event, $target) {
+  const $itemList = $target.closest("ul");
+  const $item = $target.closest("li");
+  for (let i = 0; i < $itemList.childNodes.length; i++) {
+    if ($item === $itemList.childNodes[i]) {
+      return i;
+    }
+  }
 }
 
 new TodoApp();
