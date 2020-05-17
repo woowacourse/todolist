3ee9a8e7f563e2d4bed7d3b7c666955f21995d55
jaeju.jang <jjj0611@gmail.com>
2020-05-17 22:09:40 +0900

    [feat] todo counter 구현

--- js/TodoApp.js
+++ js/TodoApp.js
@@ -1,37 +1,69 @@
 import TodoInput from "./TodoInput.js";
 import TodoList from "./TodoList.js";
 import TodoItem from "./TodoItem.js";
+import TodoCounter from "./TodoCounter.js";
+import { ITEM_STATE } from "./constants.js";
 
 function TodoApp() {
   let nextId = 1;
   this.todos = [];
+  this.selected = ITEM_STATE.ALL;
+
   const todoInput = new TodoInput({
     onAdd: content => {
       const newTodo = new TodoItem({ id: nextId++, content });
       const todos = [...this.todos, newTodo];
-      this.setState(todos);
-    }
-  })
-  const todoList = new TodoList({
-    onRemove: id => {
-      const todos = this.todos.filter(todo => todo.id !== id);
-      this.setState(todos);
-    },
-    onUpdate: (id, content) => {
-      const todos = this.todos.map(todo => todo.id === id ? new TodoItem({ id, content }) : todo);
-      this.setState(todos);
-    },
-    onCompleted: id => {
-      const todos = this.todos.map(todo => todo.id === id ? new TodoItem(todo.id,
-        todo.content,
-        !todo.isCompleted) : todo);
-      this.setState(todos);
+      const selected = this.selected;
+      this.setState(todos, selected);
     }
   });
 
-  this.setState = updatedTodoItems => {
+  const todoList = new TodoList({
+    onRemove: id => {
+      const todos = this.todos.filter(todo => todo.id !== id);
+      const selected = this.selected;
+      this.setState(todos, selected);
+    },
+    onUpdate: (id, content) => {
+      const todos = this.todos.map(todo => todo.id === id ? new TodoItem({ id, content }) : todo);
+      const selected = this.selected;
+      this.setState(todos, selected);
+    },
+    onCompleted: id => {
+      const todos = this.todos.map(todo =>
+        todo.id === id ? new TodoItem({
+          id: todo.id,
+          content: todo.content,
+          isCompleted: !todo.isCompleted
+        }) : todo);
+      const selected = this.selected;
+      this.setState(todos, selected);
+    }
+  });
+
+  const todoCounter = new TodoCounter({
+    onSelect: selected => {
+      this.setState(this.todos, selected);
+    }
+  });
+
+  this.setState = (updatedTodoItems, selected) => {
     this.todos = updatedTodoItems;
-    todoList.setState(this.todos);
+    this.selected = selected;
+    const todos = selectedTodos(this.todos, selected);
+    todoList.setState(todos, selected);
+    todoCounter.setState(todos, selected);
+  }
+
+  const selectedTodos = (todos, selected) => {
+    switch (selected) {
+      case ITEM_STATE.ACTIVE:
+        return this.todos.filter(todo => todo.isCompleted === false);
+      case ITEM_STATE.COMPLETED:
+        return this.todos.filter(todo => todo.isCompleted === true);
+      default:
+        return this.todos;
+    }
   }
 }
 
---
+++ js/constants.js
@@ -0,0 +1,6 @@
+export const ITEM_STATE = {
+  ALL: "all",
+  ACTIVE: "active",
+  COMPLETED: "completed",
+  SELECTED: "selected",
+}
