ad43b419717e2459359f5a213334c2d27ebfb7ae
moonyoungCHAE <xpf_fl@naver.com>
2020-05-24 17:59:04 +0900

    feat: todo list의 x버튼을 이용해서 해당 엘리먼트를 삭제

--- static/js/TodoApp.js
+++ static/js/TodoApp.js
@@ -1,16 +1,17 @@
 import {EVENT_TYPE, KEY_CODE} from "./utils/constants.js";
-import {newTodoItem} from "./utils/templates.js";
+import {todoItemTemplate} from "./utils/templates.js";
 
 function TodoApp() {
-    this.todo_Items = [];
+    this.todo_items = [];
 
     const $input = document.querySelector("#new-todo-title");
     const $todo_list = document.querySelector("#todo-list");
 
     const onInputHandler = event => {
         if (event.code === KEY_CODE.ENTER) {
-            this.todo_Items.push(event.value);
-            $todo_list.insertAdjacentHTML('beforeend', newTodoItem($input.value));
+            this.todo_items.push($input.value);
+            this.render(this.todo_items);
+            $input.value = "";
         }
     };
 
@@ -18,13 +19,31 @@ function TodoApp() {
         // Todo : 좀 더 좋은 방식
         // Todo : node element의 차이? (parentNode, parentElement)
         if (event.target == "input.toggle") {
-            event.target.parentNode.className = "completed";
+            event.target.parentElement.className = "completed";
+        }
+    };
+
+    const onDeleteHandler = event => {
+        if (event.target.className === "destroy") {
+            const index = event.target.parentElement.getAttribute("data-index");
+            this.todo_items = this.todo_items.slice(parseInt(index));
+            this.render(this.todo_items);
         }
     };
 
     const initEventListeners = () => {
         $input.addEventListener(EVENT_TYPE.KEY_DOWN, onInputHandler);
         $todo_list.addEventListener(EVENT_TYPE.CLICK, onCheckHandler);
+        $todo_list.addEventListener(EVENT_TYPE.CLICK, onDeleteHandler);
+    };
+
+    // Todo : 수정할 때마다 다 지우고 다시 render 해야 할까?
+    this.render = function (todo_items) {
+        const template = [];
+        todo_items.forEach( function (item, index, array) {
+          template.push(todoItemTemplate(item, index));
+        });
+        $todo_list.innerHTML = template.join("");
     };
 
     this.init = () => {
--- static/js/utils/templates.js
+++ static/js/utils/templates.js
@@ -1,7 +1,9 @@
-export const newTodoItem = item => `
-        <div class="view">
+export const todoItemTemplate = (item, index) => `
+    <li>
+        <div class="view" data-index=${index}>
           <input class="toggle" type="checkbox">
-          <label class="label"></label>
+          <label class="label">${item}</label>
           <button class="destroy"></button>
         </div>
-        <input class="edit" value=${item}>`
+        <input class="edit" value=${item}>
+  </li>`
