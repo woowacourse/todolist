45d46ddddecee4aa34706838f98a004113496881
sujin <sujinemilycho@gmail.com>
2020-05-31 15:01:14 +0900

    feat: TodoCount 구현
    - index.html에 샘플로 있던 todoItems 삭제

--- js/TodoApp.js
+++ js/TodoApp.js
@@ -1,6 +1,7 @@
 import { TodoInput } from './TodoInput.js';
 import { TodoList } from './TodoList.js';
 import { TodoItem } from './TodoItem.js';
+import { TodoCount } from './TodoCount.js';
 
 function TodoApp() {
   this.todoItems = [];
@@ -8,6 +9,7 @@ function TodoApp() {
   this.setState = updatedItems => {
     this.todoItems = updatedItems; // todo: 이 코드 필요한가?
     todoList.setState(this.todoItems);
+    todoCount.render(this.todoItems.length);
   };
 
   new TodoInput({
@@ -42,6 +44,8 @@ function TodoApp() {
       this.setState(this.todoItems);
     }
   });
+
+  const todoCount = new TodoCount();
 }
 
 const todoApp = new TodoApp();
--- js/TodoCount.js
+++ js/TodoCount.js
@@ -1,3 +1,7 @@
 export function TodoCount() {
+  this.$todoCount = document.querySelector(".todo-count");
 
+  this.render = count => {
+    this.$todoCount.innerHTML = `총 ${count} 개`;
+  }
 }
