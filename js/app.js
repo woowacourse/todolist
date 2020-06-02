353cc802d32e1e313fccebf6f006bd5dbc4a4d74
kuenhwi <kueniyam@naver.com>
2020-06-02 19:13:15 +0900

    feat: 기본적인 component 구조 구현

---
+++ js/app.js
@@ -0,0 +1,47 @@
+import {todoItemTemplate} from "./templates/template.js";
+
+function TodoApp() {
+    this.addItem = item => {
+        this.todoItems.push(item);
+        this.todoList.render(this.todoItems);
+    };
+
+    this.init = () => {
+        this.$todoInput.addEventListener("keydown", this.todoInput.addTodoItem);
+    };
+
+    this.todoItems = [];
+
+    this.$todoList = document.querySelector("#todo-list");
+    this.$todoInput = document.querySelector("#new-todo-title");
+
+    this.todoList = new TodoList(this.$todoList);
+    this.todoInput = new TodoInput(this.addItem);
+}
+
+function TodoList($todoList) {
+    this.render = items => {
+        const template = items.map(todoItemTemplate);
+        $todoList.innerHTML = template.join("");
+    };
+}
+
+function TodoInput(onAdd) {
+    this.addTodoItem = event => {
+        const $newTodoItem = event.target;
+
+        if (!this.isValid(event, $newTodoItem.value)) {
+            return;
+        }
+
+        onAdd({title: $newTodoItem.value});
+        $newTodoItem.value = "";
+    };
+
+    this.isValid = (event, todoTargetValue) => {
+        return event.key === "Enter" && todoTargetValue;
+    };
+}
+
+const todoApp = new TodoApp();
+todoApp.init();
---
+++ js/templates/template.js
@@ -0,0 +1,3 @@
+export const todoItemTemplate = item => {
+    return `${item.title}`
+};
