7822d25d102cb0af99b696d37f1ec50f449c9fc5
minuyim <minu_yim@naver.com>
2020-06-09 13:49:14 +0900

    feat: ajax 통신 기능 구현

--- js/app.js
+++ js/app.js
@@ -1,66 +1,69 @@
 import {renderTodo} from "./templete.js"
+import api from "./api.js"
+import {ALERT_MESSAGE, EVENT_TYPE, KEY_TYPE, FILTER} from "./Constant.js";
 
 function TodoApp() {
   this.todoItems = [];
   this.filter = "all";
   this.todoList = new TodoList({
-    onRemove: contents => {
-      const index = this.todoItems.findIndex(it => JSON.stringify(it) === JSON.stringify(contents));
-      this.todoItems.splice(index, 1);
-      this.setState(this.todoItems);
+    onRemove: async id => {
+      await api.app.delete(id);
+      this.setState();
     },
-    onUpdate: (contents, update) => {
-      const item = this.todoItems.find(it => JSON.stringify(it) === JSON.stringify(contents));
-      item.state = update.state;
-      item.value = update.value;
-      this.setState(this.todoItems);
+    onToggle: async (id) => {
+      await api.app.toggle(id);
+      this.setState();
     }
   });
   this.todoCount = new TodoCount({
     onSelectFilter: filter => {
       this.filter = filter;
-      this.setState(this.todoItems);
+      this.setState();
     }
   });
 
-  this.setState = updatedItems => {
-    this.todoItems = updatedItems;
+  this.setState = async () => {
+    this.todoItems = await api.app.get();
     this.todoList.setState(display(this.filter));
     this.todoCount.setState(display(this.filter));
   };
 
   const display = filter => this.todoItems.filter(item => {
-    if (filter === "active") {
-      return item.state === "undo";
-    } else if (filter === "completed") {
-      return item.state === "completed";
-    } else {
-      return true;
+    for (let f in FILTER) {
+      let filterElement = FILTER[f];
+      if (filter === filterElement.name) {
+        return filterElement.filtering(item);
+      }
     }
   });
 
+  this.setState();
+
   new TodoInput({
-    onAdd: contents => {
-      this.todoItems.push(new TodoItem("undo", contents));
-      this.setState(this.todoItems);
+    onAdd: async contents => {
+      const newData = {
+        content: contents,
+      };
+      await api.app.create(newData);
+      this.setState(await api.app.get());
     }
-  })
+  });
 }
 
 function TodoInput({onAdd}) {
   const $todoInput = document.querySelector("#new-todo-title");
   const addItem = event => {
     const $target = event.target;
-    if (event.key === "Enter" && $target.value !== "") {
+    if (event.key === KEY_TYPE.ENTER && $target.value !== "") {
       onAdd($todoInput.value);
       $todoInput.value = "";
     }
   };
 
-  $todoInput.addEventListener("keydown", addItem);
+  $todoInput.addEventListener(EVENT_TYPE.KEYDOWN, addItem);
 }
 
-function TodoList({onRemove, onUpdate}) {
+function TodoList({onRemove, onToggle}) {
   const $todoList = document.querySelector("#todo-list");
   const render = updatedItems => {
     const renderedItems = updatedItems.map(renderTodo);
@@ -73,52 +76,20 @@ function TodoList({onRemove, onUpdate}) {
 
   const removeItem = event => {
     const $target = event.target;
-    if ($target.classList.contains("destroy") && confirm("정말 삭제하시겠습니까?")) {
+    if ($target.classList.contains("destroy") && confirm(ALERT_MESSAGE.CONFIRM)) {
       const $todoItem = $target.closest("li");
-      onRemove(new TodoItem($todoItem.className, $todoItem.querySelector(".label").innerText));
-    }
-  };
-  const editItem = event => {
-    const $target = event.target;
-    if ($target.classList.contains("label") && !document.querySelector(".editing")) {
-      const $todoItem = $target.closest("li");
-      $todoItem.classList.toggle("editing");
-    }
-  };
-  const updateItem = event => {
-    const $target = event.target;
-    if (event.key === "Enter") {
-      const updatedValue = $target.value;
-      const $todoItem = $target.closest("li");
-      $todoItem.classList.toggle("editing");
-      onUpdate(new TodoItem($todoItem.className, $todoItem.querySelector(".label").innerText),
-        new TodoItem($todoItem.className, updatedValue));
+      onRemove($todoItem.dataset.id);
     }
   };
   const updateState = event => {
     const $target = event.target;
     if ($target.classList.contains("toggle")) {
       const $todoItem = $target.closest("li");
-      const className = $todoItem.className;
-      $todoItem.classList.toggle("undo");
-      $todoItem.classList.toggle("completed");
-      onUpdate(new TodoItem(className, $todoItem.querySelector(".label").innerText),
-        new TodoItem($todoItem.className, $todoItem.querySelector(".label").innerText));
+      onToggle($todoItem.dataset.id);
     }
   };
-  const cancelUpdate = event => {
-    const $target = event.target;
-    if (event.key === "Escape") {
-      const $todoItem = $target.closest("li");
-      $todoItem.querySelector(".edit").value = $todoItem.querySelector("label").innerText;
-      $todoItem.classList.toggle("editing");
-    }
-  };
-  $todoList.addEventListener("click", removeItem);
-  $todoList.addEventListener("click", updateState);
-  $todoList.addEventListener("dblclick", editItem);
-  $todoList.addEventListener("keydown", updateItem);
-  $todoList.addEventListener("keydown", cancelUpdate);
+  $todoList.addEventListener(EVENT_TYPE.CLICK, removeItem);
+  $todoList.addEventListener(EVENT_TYPE.CLICK, updateState);
 }
 
 function TodoCount({onSelectFilter}) {
@@ -139,12 +110,7 @@ function TodoCount({onSelectFilter}) {
     }
   };
 
-  $filterList.addEventListener("click", selectFilter);
-}
-
-function TodoItem(state, value) {
-  this.state = state;
-  this.value = value;
+  $filterList.addEventListener(EVENT_TYPE.CLICK, selectFilter);
 }
 
 new TodoApp();
--- js/templete.js
+++ js/templete.js
@@ -1,11 +1,12 @@
 export function renderTodo(todo) {
-  const checked = todo.state === "completed" ? "checked" : "";
-  return `<li class=${todo.state}>
+  const checked = todo.isCompleted ? "checked" : "";
+  const state = todo.isCompleted ? "completed" : "undo";
+  return `<li class=${state} data-id=${todo._id}>
   <div class="view">
     <input class="toggle" type="checkbox" ${checked}>
-    <label class="label">${todo.value}</label>
+    <label class="label">${todo.content}</label>
   <button class="destroy"></button>
     </div>
-    <input class="edit" value=${todo.value}>
+    <input class="edit" value=${todo.content}>
     </li>`
 }
