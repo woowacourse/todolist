cbf299ab3e8aeef24432eff693bf72909691fcf5
Sony <sonypark0204@gmail.com>
2020-06-10 20:08:34 +0900

    refactor: TodoFilterTemplate 파일로 분리

--- js/TodoFilter.js
+++ js/TodoFilter.js
@@ -1,4 +1,5 @@
-import { EVENT, STATUS } from './utils/constants.js'
+import { EVENT } from './utils/constants.js'
+import { todoFilterTemplate } from './TodoFilterTemplate.js';
 
 export default function TodoFilter({ onSelect }) {
   const $filters = document.querySelector(".filters")
@@ -13,17 +14,4 @@ export default function TodoFilter({ onSelect }) {
     const $target = event.target
     onSelect($target.dataset.select)
   }
-
-  const todoFilterTemplate = selected =>
-    `   
-      <li>
-        <a class="${selected === STATUS.ALL ? "all selected" : "all"}" href="#/" data-select="${STATUS.ALL}">전체보기</a>
-      </li>
-      <li>
-        <a class="${selected === STATUS.ACTIVE ? "active selected" : "active"}" href="#/active" data-select="${STATUS.ACTIVE}">해야할 일</a>
-      </li>
-      <li>
-        <a class="${selected === STATUS.COMPLETED ? "completed selected" : "completed"}" href="#/completed" data-select="${STATUS.COMPLETED}">완료한 일</a>
-      </li>
-    `
 }
