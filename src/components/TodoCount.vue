2179e154078bfd6a284b2c39f553d1f64e349617
K.S.KIM <nullable@kakao.com>
2020-07-09 19:20:04 +0900

    feat: TodoCount 추가

---
+++ src/components/TodoCount.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="count-container">
+        <span class="todo-count">
+            총
+            <strong>{{ count }}</strong> 개
+        </span>
+        <ul class="filters">
+            <todo-filter v-for="filter in filters" :key="filter.className" :filterType="filter" />
+        </ul>
+    </div>
+</template>
+
+<script>
+import TodoFilter from './TodoFilter.vue';
+import { FilterType } from '../util/constants.js';
+
+export default {
+    components: {
+        TodoFilter,
+    },
+    computed: {
+        count() {
+            return this.$store.getters.filteredCount;
+        },
+        filters() {
+            return FilterType;
+        },
+    },
+};
+</script>
