fb2b1adddc9c0fb0108d44e5a550a5125a221be7
K.S.KIM <nullable@kakao.com>
2020-07-09 19:19:11 +0900

    feat: TodoFilter 추가

---
+++ src/components/TodoFilter.vue
@@ -0,0 +1,23 @@
+<template>
+    <li>
+        <a class="{ filterType.className }" :class="{ selected }" @click="changeFilter">{{ filterType.content }}</a>
+    </li>
+</template>
+
+<script>
+export default {
+    props: {
+        filterType: Object,
+    },
+    computed: {
+        selected() {
+            return this.$store.state.filter.className === this.filterType.className;
+        },
+    },
+    methods: {
+        changeFilter() {
+            this.$store.commit('CHANGE_FILTER', this.filterType);
+        },
+    },
+};
+</script>
