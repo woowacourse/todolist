18879b3dc9a075bd8a68eea442a05df478091064
K.S.KIM <nullable@kakao.com>
2020-07-09 19:20:23 +0900

    feat: TodoList 추가

---
+++ src/components/TodoList.vue
@@ -0,0 +1,23 @@
+<template>
+    <ul id="todo-list" class="todo-list">
+        <todo-item v-for="todo in todos" :key="todo._id" :todo="todo" />
+    </ul>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue';
+
+export default {
+    components: {
+        TodoItem,
+    },
+    created() {
+        this.$store.commit('LOAD_TODOS');
+    },
+    computed: {
+        todos() {
+            return this.$store.getters.filteredTodos;
+        },
+    },
+};
+</script>
