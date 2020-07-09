fb3fc3efa0dbf0b98c9d892c43caf93476fff3c3
K.S.KIM <nullable@kakao.com>
2020-07-09 19:20:15 +0900

    feat: TodoInput 추가

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,31 @@
+<template>
+    <input
+        id="new-todo-title"
+        class="new-todo"
+        placeholder="할일을 추가해주세요"
+        autofocus
+        @keyup.enter="addTodo"
+        v-model="content"
+    />
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            content: '',
+        };
+    },
+    methods: {
+        addTodo() {
+            const newContent = this.content;
+            if (newContent.trim().length === 0) {
+                alert('입력값이 올바르지 않습니다.');
+                return;
+            }
+            this.content = '';
+            return this.$store.commit('ADD_TODO', newContent);
+        },
+    },
+};
+</script>
